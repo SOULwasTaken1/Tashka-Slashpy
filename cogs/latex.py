7df5dabde7e2a17f06d05b59bff57fa6c91817c4
VOID SOUL <80249387+SOULwasTaken1@users.noreply.github.com>
2023-10-21 12:47:16 +0600

    Made latex.py better and changed the fonts
    I couldn't change the font using matplotlib. The font was boring, so I wanted to make it look more algebraic. I couldn't use any custom fonts or change the code to switch fonts. Then I found latex's in-built function \mathscr{} which makes math texts look more algebraic. But I couldn't just put the entire latex code inside that function because it breaks when ^ or _  or \ are inside. Plus it changed stuff like sin, cot, tan. That's why I had to change the code so much

--- cogs/latex.py
+++ cogs/latex.py
@@ -6,17 +6,81 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as font_manager
 import os
+from matplotlib import rcParams
 from typing import Optional
 import numpy as np
 import re
 
+def break_latex_code(latex_code, banned_list):
+  pattern = r'(\{[^}]*\})|([a-zA-Z0-9]+)|(\W)'
+  elements = re.split(pattern, latex_code)
+
+  cleaned_elements = []
+  for element in elements:
+    if element:
+      if any(banned in element for banned in banned_list):
+          element = element
+      else:
+        element = element.replace('{', '').replace('}', '')
+      cleaned_elements.append(element)
+
+  return cleaned_elements
+
+def break_exponent(input_string, break_point):
+  pattern = r"|".join(map(re.escape, break_point))
+  pattern = f"({pattern})"
+
+  result = re.split(pattern, input_string)
+  result = [item for item in result if item]
+
+  return result
+
+
+
+def modify_latex_string(elements, banned_list, break_point):
+  result = []
+  for i, item in enumerate(elements):
+    modified_item = ''
+    o1 = ['^', '_']
+    o2 = ['dfrac', 'frac']
+    if elements[i - 1] in o1 or elements[i-1] in o2 or elements[i-2] in o2:
+        arr = break_exponent(item,break_point)
+
+        if '}' in arr :  arr.remove('}')
+        if '{' in arr : arr.remove('{')
+        res = []
+        mod_item = ''
+        for i, item in enumerate(arr):
+          if any(banned in item for banned in break_point):
+            mod_item = item
+          else:
+            mod_item = r'\mathscr{' + item + '}'
+          res.append(mod_item)
+        modified_item = '{' + ''.join(res) + '}'
+
+    elif any(banned in item for banned in banned_list):
+
+      modified_item = item
+    elif elements[i-1] == 'dfrac':
+      print(elements[i])
+    elif elements[i] not in banned_list and elements[i-1] not in '_^':
+        modified_item = r'\mathscr{' + item + '}'
+    else:
+        modified_item = item
+    result.append(modified_item)
+
+
+  return ''.join(result)
+
 
 def latex_to_png(latex_str,size):
-  
 
+  font = {'family': 'serif',
+    'style': 'italic'}
+  
   fig = plt.figure(facecolor='black')
   plt.axis("off")
-  plt.text(0.5,0.5,f"${latex_str}$",size=size,ha="center",va="center",color="white",)
+  plt.text(0.5,0.5,f"${latex_str}$",size=size,ha="center",va="center",color="white",fontdict=font)
 
   png_path = "result.png"
 
@@ -67,19 +131,72 @@ class latex(commands.Cog,name='latex'):
                    size:Optional[int]=100,
                    ephemeral:Optional[bool]=False
                   ):
-
     
+    banned_list = ['^', '_', '\tan','\cos', '\sin',
+     '\log', '\ln', 'e', '\\pi','\sec', r'\to', '\propto'
+     '\csc', '\cot', '\arcsin', '\arccos', r'\dfrac', 'dfrac'
+     '\arctan', '\arccsc', '\\', '{', '}',  '\frac', 'frac', 'int',
+     '\\sum', '\\int', '\\lim', '\l\imsup', '\\prod', 'lim',
+     '\\liminf', '\\liminfsup', '\\liminfinf', '+', '-', ' ', '='
+     '\\infty', '\\inftysim', '\\inftyn', '\\inftyne', '\\inftyne',
+     '\\alpha', '\\beta', '\\gamma', '\\delta', '\\epsilon', '\\zeta',
+     '\\eta', '\theta', '\iota', '\kappa', '\lambda', '\\mu', r'\nu',
+     '\\xi', '\\omicron', '\\rho', '\\sigma' , r'\\tau', '\\upsilon', '\\Omega',
+     '\\phi', '\\chi', '\psi', '\\omega', '\\Gamma', '\\Delta', '\\Theta',
+     '\\Lambda', '\\Xi', '\\Pi', '\\Sigma', '\\Upsilon', '\\Phi', '\\Psi',
+     r'\nless', r'\\nlesseqq', '\\nge', 'r\\ngeq', '\\lesseqq', '\subset',
+     '\\supset', '\\subseteq', '\\supseteq', 'r\\nsubset', r'\N', '\R',
+     '\Q', '\C', '\Z', '\forall' , '\\exists', '\\nexists', '\\varnothing',
+     '\aleph', '\sinh', '\cosh', '\tanh', '\cot', '\coth', '\sech', '\csch',
+     '\arcsinh', '\arccosh', '\arctanh', '\arccoth', '\arcsech', '\apporx', '(', ')'
+      ]
+    # break points for the function
+    break_point = ['sec', 'csc', 'cot', 'sin', 'cos', r'tan', 'sec', 'csc',
+       'arcsec', 'arccsc', 'arccot', 'arcsin', 'arccos', 'arctan',
+      r'\to', r'\infty', 'ln', 'log', '\alpha', '\beta', '\gamma',
+      r'\delta', r'\epsilon', '\zeta', '\eta', r'\theta', r'\iota',
+      r'\kappa', '\lambda', '\mu', '\nu', '\omicron', '\rho','{', '}',
+      '\sigma', r'\tau', r'\upsilon', r'\Omega', '\phi', '\chi', 'r\psi',
+      '\omega', '\Gamma', '\Delta', '\Theta', '\Lambda', '\Xi',
+      '\Pi', '\Sigma', r'\Upsilon', r'\Phi', r'\Psi', '\aleph',
+      ]
+
     msg = await ctx.respond("please wait",ephemeral=ephemeral)
     try:
-      latex_to_png(prompt,size)
+      
+      replace_dict = {
+          "*": "\cdot",
+          "tan": r"\tan",
+          "cos": "\cos",
+          "sin": "\sin",
+          "sec": "\sec",
+          "csc": "\csc",
+          "cot": "\cot",
+          "arcsin": "\arcsin",
+          "arccos": "\arccos",
+          "arctan": "\arctan",
+          "arccsc": "\arccsc",
+          "arccot": "\arccot",
+          "arcsec": "\arcsec",
+
+      }
+      
+        
+      elements = break_latex_code(prompt, banned_list)
+      modified_latex = modify_latex_string(elements, banned_list, break_point)
+      for old, new in replace_dict.items():
+        modified_latex = modified_latex.replace(old, new)
+       
+      latex_to_png(modified_latex,size)
       path = "result.png"
       make_small(path,70)
       with open('result.png', 'rb') as f:
               image = discord.File(f)
       await msg.edit_original_response(content="",file=image)
       os.remove("result.png")
-    except:
-      await msg.edit_original_response(content="The LaTeX code you sent was invalid. Please check again.",ephemeral=ephemeral)
+    except Exception as err:
+      await msg.edit_original_response(content=f"The LaTeX code you sent was invalid. Please check again.\n```hs\n {err}``` ")
+    
 
 
 
